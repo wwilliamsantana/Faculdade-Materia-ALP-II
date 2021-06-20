e7f031636fca327c678c774c2ba4c98f9e8c00f8
William Santana <84254929+wwilliamsantana@users.noreply.github.com>
2021-06-20 14:07:53 -0300

    Rename Unidade 2/Problema 1 -Divisível por 5.cpp to Unidade 2/Problema/Problema 1 -Divisível por 5.cpp

--- Unidade 2/Problema/Problema 1 -Divisível por 5.cpp
+++ Unidade 2/Problema/Problema 1 -Divisível por 5.cpp
@@ -8,14 +8,14 @@ int main(){
 
 	int num;
 	
-	printf("Digite um número: ");
+	printf("Digite um nÃºmero: ");
 	scanf("%d", &num);
 	
 	if (num % 5 == 0){
-		printf("O número %d é divisível por 5", num);
+		printf("O nÃºmero %d Ã© divisÃ­vel por 5", num);
 	}
 	else { 
-		printf("O número %d não é divisível por 5.", num);
+		printf("O nÃºmero %d nÃ£o Ã© divisÃ­vel por 5.", num);
 	}
 	
 	return(0);
